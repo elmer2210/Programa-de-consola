6a99208c98cea040a529d54aefa13ad930f10094
Elmer Rivadeneira <51166398+elmer2210@users.noreply.github.com>
2020-06-24 16:57:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tarea
+{
+    class DescripcionPersonal
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length>0){
+                Console.WriteLine(args[0]);
+            }else{
+                Console.WriteLine("No existen argumentos");
+            }
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre =  Console.ReadLine();
+            Console.WriteLine("Carrera:");
+            string carrera =  Console.ReadLine();
+            Console.WriteLine($"Hola mi nombre es {nombre}, y estudio en la facultad {carrera}");
+            Console.ReadKey(); 
+        }
+    }
+}
